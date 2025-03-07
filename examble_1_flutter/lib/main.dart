d44d253c6d9980dee0827d3d6f9b908c9b874236
Ahmed Salem <129005777+ahmedsalem2002m@users.noreply.github.com>
2025-03-07 16:50:35 +0200

    Done task_1 Flutter

--- examble_1_flutter/lib/main.dart
+++ examble_1_flutter/lib/main.dart
@@ -11,19 +11,129 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.lightBlue,
-          title: Text(
-            "Ali",
-            style: TextStyle(
-              fontWeight: FontWeight.w900,
-              fontSize: 30,
-              color: Colors.deepPurple
-            ),
+        body: Container(
+          width: 300,
+          margin: EdgeInsets.all(30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 30),
+              Text(
+                "Power Consistency",
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.blueGrey[400],
+                ),
+              ),
+              SizedBox(height: 30),
+              Text(
+                textAlign: TextAlign.center,
+                "Consistency is the key to success in any aspect of life. Whether it’s learning a new skill, building a business, or improving personal habits, small, repeated efforts lead to significant results over time. Many people start with enthusiasm but give up when they don’t see immediate progress.",
+                style: TextStyle(
+                  fontSize: 20,
+                  //fontWeight: FontWeight.w500,
+                  color: Colors.blueGrey,
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.star, color: Colors.blueGrey[300]),
+                      Icon(Icons.star, color: Colors.blueGrey[300]),
+                      Icon(Icons.star, color: Colors.blueGrey[300]),
+                      Icon(Icons.star, color: Colors.blueGrey[300]),
+                      Icon(Icons.star, color: Colors.blueGrey[300]),
+                    ],
+                  ),
+                  Text(
+                    "170 Reviews",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.account_balance_wallet_rounded,
+                        size: 24,
+                        color: Colors.green[200],
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "PREP:",
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                          "25 min",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.access_alarm_sharp,
+                        size: 24,
+                        color: Colors.green[200],
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "COOK:",
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                          "1 hr",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.food_bank_rounded,
+                        size: 24,
+                        color: Colors.green[200],
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "FEEDS:",
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                          "4-6",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
